e7bede9cb85e2e7028cfa61a4dd67d1f72cc31a4
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-09-13 10:06:16 +0530

    initial files

---
+++ JQueryDatePicker/JQueryDatePicker/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JQueryDatePicker.Models;   
+
+namespace JQueryDatePicker.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            Customer cust = new Customer();
+            cust.JoinDate = DateTime.Now.ToShortDateString();   
+            return View(cust);
+        }
+
+        [HttpPost] 
+        public ActionResult Index(Customer cust)
+        {
+            string joinDate = cust.JoinDate;
+            return View();
+        }
+
+    }
+}
---
+++ JQueryDatePicker/JQueryDatePicker/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; 
+
+namespace JQueryDatePicker.Models
+{
+    public class Customer
+    {
+        [Display(Name="Join Date")] 
+        public string JoinDate { get; set; }
+    }
+}
